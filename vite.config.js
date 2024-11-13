cccbdc6993844cc5552eb3062643168159b3379c
MarufMahadhi <marufmahadhi683411@gmail.com>
2024-11-13 16:47:01 +0600

    solved

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build:{
+        rollupOptions:{
+            input:{
+                main: resolve( __dirname, "index.html"),
+                main: resolve( __dirname, "mala.html"),
+            }
+        }
+    }
+}
+
+)
